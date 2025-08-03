c2a942e0c3921d575cf911461e36ec5db419959e
Patel Dev <pateldev6622@gmail.com>
2025-08-03 16:21:42 +0530

    javascript practice

--- Javascript/DOM/section 16/Change iner html/script.js
+++ Javascript/DOM/section 16/Change iner html/script.js
@@ -1,1 +1,24 @@
-document.querySelector("h1").innerHTML = "Hello Dev Patel!!"
+// Store elements in variables
+const div = document.querySelector("div");
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+
+// Magic toggle function
+function magic() {
+    if (div.style.backgroundColor === "black") {
+        div.style.backgroundColor = "white";
+        button.style.backgroundColor = "black";
+        button.style.color = "white";
+    } else {
+        div.style.backgroundColor = "black";
+        button.style.backgroundColor = "white";
+        button.style.color = "black";
+    }
+}
+
+// Initial setup
+h1.innerHTML = "Hello Dev Patel!!";
+h1.style.backgroundColor = "seagreen";
+
+// Add event listener (note: no parentheses after magic)
+button.addEventListener("click", magic);
---
+++ Projects/Drum Kit Starting Files/index.js
@@ -0,0 +1,21 @@
+document.querySelector(".w").addEventListener("click",function () {
+    document.getElementById("clickSoundw").play();
+})
+document.querySelector(".a").addEventListener("click",function () {
+    document.getElementById("clickSounda").play();
+})
+document.querySelector(".s").addEventListener("click",function () {
+    document.getElementById("clickSounds").play();
+})
+document.querySelector(".d").addEventListener("click",function () {
+    document.getElementById("clickSoundd").play();
+})
+document.querySelector(".j").addEventListener("click",function () {
+    document.getElementById("clickSoundj").play();
+})
+document.querySelector(".k").addEventListener("click",function () {
+    document.getElementById("clickSoundk").play();
+})
+document.querySelector(".l").addEventListener("click",function () {
+    document.getElementById("clickSoundl").play();
+}) 
